0dafa8701c2eead3e6ebd277a8306b3456594679
Abhinav9164 <abhinav6981@gmail.com>
2023-03-19 04:06:44 +0530

    1 commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Headers/Header';
+import Body from './components/Body/Body';
+
+import './App.css';
+import LoginNav from './components/LoginNav/Login';
+
+
+function App() {
+  return ( 
+    <div className="App">
+      <LoginNav/>
+      <Header/>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginNav/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Login.module.css"
+
+function LoginNav(){
+    return(
+<div className={styles.container}>
+<button>Login</button>
+</div>
+
+    )
+}
+
+export default LoginNav;
